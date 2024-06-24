2b8ac4a7de9286b8dda121680221169ccafcaf1b
MinhPhu <minhphuzcod@gmail.com>
2024-06-24 20:57:44 +0700

    final1

--- CDJava/src/seminal/pack1/Exc39_Test.java
+++ CDJava/src/seminal/pack1/Exc39_Test.java
@@ -1,14 +1,6 @@
 package seminal.pack1;
 
-public class Exc39_Test extends Exc39 {
+public class Exc39_Test {
 
-	int test() {
-		return i;
-	}
-
-	public static void main(String[] args) {
-		Exc39_Test t = new Exc39_Test();
-		System.out.println(t.test());
-	}
-
+	float[] a = new float[3];
 }
