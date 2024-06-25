0751b6603e8348dd0f5465455ef52ed01f27c5a5
MinhPhu <minhphuzcod@gmail.com>
2024-06-25 23:37:27 +0700

    final3

--- CDJava/src/final_1/Test2_duplicate.java
+++ CDJava/src/final_1/Test2_duplicate.java
@@ -1,8 +1,21 @@
 package final_1;
 
 public class Test2_duplicate {
-	
+	static int y = 2;
+
+	public Test2_duplicate(int x) {
+		this();
+		y *= 3;
+	}
+
 	public Test2_duplicate() {
-		System.out.print("Alpha ");
+		y += 5;
+	}
+
+	public static void main(String[] args) {
+		new Test2_2();
+		Test2_2 test2_2 = new Test2_2();
+		System.out.println(test2_2.toString());
+		System.out.println("Test");
 	}
 }
